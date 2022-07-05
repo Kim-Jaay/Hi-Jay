8e0b45297b9b6e23bdfa55997d9c97d6c95f5e1e
Jay <jjjjuhyuneee@naver.com>
2022-07-05 21:22:44 +0900

    0705-7

--- 02.salady/js/main.js
+++ 02.salady/js/main.js
@@ -8,4 +8,21 @@ $(function () {
         vertical: true,
         verticalSwiping: true,
     });
-})
+
+
+    $('.main_slide').on('init afterChange', function (e, s, c) {
+        var current = $('.slick-current');
+        current.addClass('on').siblings().removeClass('on');
+
+        $('.slide_num').text((c ? (c + 1) : 1) + " / " + s.slideCount);
+    });
+
+    $('.main_arrow i:nth-child(1)').on('click', function () {
+        $('.main_slide').slick('slickPrev');
+    });
+
+    $('.main_arrow i:nth-child(2)').on('click', function () {
+        $('.main_slide').slick('slickNext');
+    });
+
+});
