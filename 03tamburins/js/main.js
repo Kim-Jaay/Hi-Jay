85d15482c4dd11374269fcafa7a0486a08d32a02
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-07-13 10:08:20 +0900

    0713-01

--- 03tamburins/js/main.js
+++ 03tamburins/js/main.js
@@ -1,8 +1,32 @@
 $(function () {
+
+    $('.main_slider').on('init afterChange', function (e, s, c) {
+        var current = $('.slick-current');
+        current.addClass('on').siblings().removeClass('on');
+        $('.slide_num').text((c ? (c + 1) : 1) + " / " + s.slideCount);
+
+
+        $('.main_arrow').removeClass('on');
+        if (c === 0) {
+            $('.main_arrow').addClass('on')
+        }
+    });
+
+
     $('.main_slider').slick({
         arrows: false,
         autoplay: true,
         pauseOnHover: false,
         pauseOnFocus: false,
+        fade: true,
+    });
+
+
+    $('.main_arrow .prev').on('click', function () {
+        $('.main_slider').slick('slickPrev');
+    });
+
+    $('.main_arrow .next').on('click', function () {
+        $('.main_slider').slick('slickNext');
     });
 });
