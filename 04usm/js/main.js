7a110516c70198ebb3859d378af975900679503d
Kim-Jaay <you@example.com>
2022-07-22 17:30:57 +0900

    0722-03

--- 04usm/js/main.js
+++ 04usm/js/main.js
@@ -1,4 +1,12 @@
 $(function () {
-
+    $('.at_home_slide').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        slidesToShow: 3
+        ,
+        centerMode: true,
+    });
 
 });
