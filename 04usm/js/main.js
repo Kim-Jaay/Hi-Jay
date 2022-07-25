b74c61b8568e025e7865c3c43f654346a540a4a2
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-07-25 17:16:50 +0900

    0725-01

--- 04usm/js/main.js
+++ 04usm/js/main.js
@@ -4,9 +4,31 @@ $(function () {
         autoplay: true,
         pauseOnHover: false,
         pauseOnFocus: false,
-        slidesToShow: 3
-        ,
+        slidesToShow: 3,
         centerMode: true,
     });
 
+    $('.slide01').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        autoplaySpeed: 0,
+        speed: 3000,
+        vertical: true,
+        slidesToShow: 3,
+        cssEase: "linear",
+    });
+
+    $('.slide02').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        autoplaySpeed: 0,
+        speed: 3000,
+        vertical: true,
+        slidesToShow: 3,
+        cssEase: "linear",
+    });
 });
