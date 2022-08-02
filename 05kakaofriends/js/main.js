66c9a69ef82149a990954b864828b2dfd90c451e
Jay <jjjjuhyuneee@naver.com>
2022-08-02 21:54:43 +0900

    0802

--- 05kakaofriends/js/main.js
+++ 05kakaofriends/js/main.js
@@ -2,4 +2,16 @@ $(function () {
     $('.topbanner i').on('click', function () {
         $('.topbanner').slideUp();
     })
+
+    $('.main_slider').slick({
+        arrows: false,
+        autoplay: true,
+        dots: false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        fade: true,
+    });
+
+
+
 })
