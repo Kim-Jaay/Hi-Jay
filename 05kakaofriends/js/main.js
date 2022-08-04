46c0114742c99211aa71e57322294c36355f1055
Jay <jjjjuhyuneee@naver.com>
2022-08-04 21:54:08 +0900

    0804

--- 05kakaofriends/js/main.js
+++ 05kakaofriends/js/main.js
@@ -46,4 +46,18 @@ $(function () {
         $('.shop_slider').slick('slickNext');
     })
 
-})
+
+    $('.to_top').on('click', function () {
+        $('html,body').animate({ scrollTop: 0 }, 200)
+    });
+
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop();
+        if (sct > 500) {
+            $('.to_top').fadeIn(300)
+        } else {
+            $('.to_top').fadeOut(1000)
+        }
+    });
+});
+
