67f200b0dfce6b8767bf0241b2e32b1bbd57abcd
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-08-24 16:50:30 +0900

    0824

---
+++ 07h&m/js/main.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    new Splide('.splide', {
+        type: 'loop',
+        arrows: boolean = false,
+        perPage: 1,
+
+    }).mount();
+
+});
