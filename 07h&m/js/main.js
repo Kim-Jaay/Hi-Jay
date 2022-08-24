e535a66eb1e2fd4f9187374b485f143fb3d014cc
Jay <jjjjuhyuneee@naver.com>
2022-08-24 22:35:01 +0900

    0824

--- 07h&m/js/main.js
+++ 07h&m/js/main.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    new Splide('.splide', {
+    const MainSlider = new Splide('.MainVisual', {
         type: 'loop',
         pauseOnHover: false,
         pauseOnFocus: false,
@@ -8,8 +8,29 @@ window.addEventListener('DOMContentLoaded', () => {
         autoplay: true,
         speed: 200,
         perPage: 1,
-        // padding: { left: '40px', right: '40px' }
+    }).mount();
 
+
+    const ProductCon = new Splide('.ProductCon', {
+        type: 'loop',
+        perPage: 4,
+        arrows: true,
+        pagination: false,
     }).mount();
 
 });
+
+{/* <script>
+    $(function () {
+        $('.tab_menu>li>a').on('click', function (e) {
+            e.preventDefault();
+            // console.log($(this).parent().index());
+            var idx = $(this).parent().index();
+            $('.tab_content>li').removeClass('on');
+            $('.tab_content>li').eq(idx).addClass('on');
+            $('.tab_menu>li').removeClass('on')
+            $(this).parent().addClass('on');
+        });
+
+
+</script> */}
