460cef0ef7e39e7cc154a57bb4f12b92ce8b1406
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-08-25 12:47:20 +0900

    0825

--- 07h&m/js/main.js
+++ 07h&m/js/main.js
@@ -67,20 +67,21 @@ window.addEventListener('DOMContentLoaded', () => {
     for (var i = 0; i < tabList.length; i++) {
         tabList[i].querySelector('.TabMenu>li>a').addEventListener('click', function (e) {
             e.preventDefault();
+
             for (var j = 0; j < tabList.length; j++) {
                 // 나머지 버튼 클래스 제거
                 tabList[j].classList.remove('on');
 
                 // 나머지 컨텐츠 display:none 처리
                 contents[j].style.display = 'none';
+
+                // 버튼 관련 이벤트
+                this.parentNode.classList.add('on');
             }
-
-            // 버튼 관련 이벤트
-            this.parentNode.classList.add('on');
-
             // 버튼 클릭시 컨텐츠 전환
-            activeCont = this.getAttribute('href');
+            activeCont = ('.TabMenu>li>a').getAttribute('href');
             document.querySelector(activeCont).style.display = 'block';
+
         });
     }
 
