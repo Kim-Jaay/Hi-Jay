d42169c362cf12960ec0d3d477af37455c1f3c50
Jay <jjjjuhyuneee@naver.com>
2022-09-02 00:14:12 +0900

    0902

--- 08studioDragon/js/main.js
+++ 08studioDragon/js/main.js
@@ -4,6 +4,30 @@ window.addEventListener('DOMContentLoaded', () => {
     //////////////////
 
 
+    const BgSlider = new Swiper('.bg_slide', {
+        loop: true,
+        effect: 'fade',
+
+    });
+
+    const PosterSlider = new Swiper('.poster_slide', {
+        loop: true,
+        slidesPerView: 9,
+        spaceBetween: 30,
+    });
+
+    document.querySelector('.bg_slide_handler .prev').addEventListener('click', () => {
+        BgSlider.slidePrev();
+    });
+    document.querySelector('.bg_slide_handler .next').addEventListener('click', () => {
+        BgSlider.slideNext();
+    });
+    document.querySelector('.bg_slide_handler .prev').addEventListener('click', () => {
+        PosterSlider.slidePrev();
+    });
+    document.querySelector('.bg_slide_handler .next').addEventListener('click', () => {
+        PosterSlider.slideNext();
+    });
 
 
 
