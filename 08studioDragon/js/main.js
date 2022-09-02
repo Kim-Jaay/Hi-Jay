c5f68d525b665939ffcab07c104bc390866362ab
Kim-Jaay <jjjjuhyuneee@naver.com>
2022-09-02 09:29:13 +0900

    0902

--- 08studioDragon/js/main.js
+++ 08studioDragon/js/main.js
@@ -12,8 +12,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const PosterSlider = new Swiper('.poster_slide', {
         loop: true,
-        slidesPerView: 9,
+        slidesPerView: 8,
         spaceBetween: 20,
+    });
+
+    const PosterBigSlider = new Swiper('.poster_slide_big', {
+        loop: true,
+        slidesPerView: 1,
+        effect: 'fade',
+
     });
 
     document.querySelector('.bg_slide_handler .prev').addEventListener('click', () => {
@@ -28,28 +35,47 @@ window.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.bg_slide_handler .next').addEventListener('click', () => {
         PosterSlider.slideNext();
     });
-
-
+    document.querySelector('.bg_slide_handler .prev').addEventListener('click', () => {
+        PosterBigSlider.slidePrev();
+    });
+    document.querySelector('.bg_slide_handler .next').addEventListener('click', () => {
+        PosterBigSlider.slideNext();
+    });
 
 
     const counters = document.querySelectorAll('.value');
     const speed = 300;
 
-    counters.forEach(counter => {
-        const animate = () => {
-            const value = +counter.getAttribute('akhi');
-            const data = +counter.innerText;
 
-            const time = value / speed;
-            if (data < value) {
-                counter.innerText = Math.ceil(data + time);
-                setTimeout(animate, 1);
-            } else {
-                counter.innerText = value;
+
+    window.addEventListener('scroll', () => {
+        let sct = window.scrollY;
+
+        console.log(sct);
+        sct > 890
+        counters.forEach(counter => {
+            const animate = () => {
+                const value = +counter.getAttribute('akhi');
+                const data = +counter.innerText;
+
+                const time = value / speed;
+                if (data < value) {
+                    counter.innerText = Math.ceil(data + time);
+                    setTimeout(animate, 1);
+                } else {
+                    counter.innerText = value;
+                }
             }
-        }
-        animate();
+            animate();
+        });
     });
+
+
+
+
+
+
+
 
 
 
